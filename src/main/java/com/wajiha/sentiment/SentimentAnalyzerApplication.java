5aee796a28b95c8331e27be081f73e756892fd2c
wajihakhan <wajihayousafzai1@gmail.com>
2026-06-15 18:45:57 +0500

    Create SentimentAnalyzerApplication.java

---
+++ src/main/java/com/wajiha/sentiment/SentimentAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.wajiha.sentiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalyzerApplication.class, args);
+	}
+}
